376fec82cf8889baeaeb5c3595c0e2d7bf99c1ba
Prev-I <claudio.previtali.88@gmail.com>
2018-10-19 12:09:28 +0200

    Read parameters from command line instead of setting file variables

--- Windows/OldFilesCleaner.bat
+++ Windows/OldFilesCleaner.bat
@@ -5,22 +5,32 @@
 :: Disable command echo
 @echo off
 
-:: Search parameters
-set destfldr="C:\Users\Public\Documents"
-set searchsubdir=TRUE
-set deleteempty=FALSE
-set filter="*.*"
-set retaindays=30
+:: Example launch from CMD
+:: OldFilesCleaner.bat "C:\Users\Public\Documents" "*.*" TRUE FALSE 30
+
+:: Example set parameters
+:: set destfldr="C:\Users\Public\Documents"
+:: set searchfltr="*.*"
+:: set searchsubdir=TRUE
+:: set deleteempty=FALSE
+:: set retaindays=30
+
+:: Script search parameters
+set destfldr=%1
+set searchfltr=%2
+set searchsubdir=%3
+set deleteempty=%4
+set retaindays=%5
 
 :: Prepare parameters for search command (semove double quote, correctly set subdirsearch, ecc...)
 set destfldr=%destfldr:"=%
-set filter=%filter:"=%
+set searchfltr=%searchfltr:"=%
 set retaindays=-%retaindays% 
 if %searchsubdir%==TRUE (set subdirpar="-s") else (set subdirpar="")  
 set subdirpar= %subdirpar:"=%
 
 :: Execute the deletion command on files that match the search parameters
-forfiles -p %destfldr% %subdirpar% -m %filter% -d %retaindays% -c "cmd /c if @isdir==FALSE (del /F /Q @file)"
+forfiles -p %destfldr% %subdirpar% -m %searchfltr% -d %retaindays% -c "cmd /c if @isdir==FALSE (del /F /Q @file)"
 
 :: If deleteempty is true remove the empty subfolders
 if %deleteempty%==TRUE (Forfiles -p %destfldr% %subdirpar% -c "cmd /c if @isdir==TRUE (rmdir /q @file)")
